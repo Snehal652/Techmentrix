239527ff84cee11da9d36bca2fd55655cd166584
Snehal Tripathi <snehalprdc2003@gmail.com>
2024-05-23 23:17:58 +0530

    main

---
+++ client/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { AuthProvider } from './store/auth.jsx'
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+  <React.StrictMode>
+    <App />
+    <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        bodyClassName="toastBody"
+        />
+  {/* Toastcontainer is for modern alert pop-up. Isko yaha isliye declare kar rhe taaki har jagah isse usse kar sake. */}
+  </React.StrictMode>
+  </AuthProvider>
+)
